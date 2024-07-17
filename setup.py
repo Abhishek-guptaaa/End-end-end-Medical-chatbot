1aa564045f04d5513e8bc8892ae7cdddfd679ae3
Abhishek <101599136+Abhishek-guptaaa@users.noreply.github.com>
2024-07-17 19:49:34 +0530

    requirements done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical Chatbot",
+    version='0.0.0',
+    author="Abhishek Gupta",
+    author_email='Abhishek18895gupta@gmail.com',
+    packages=find_packages()
+)
